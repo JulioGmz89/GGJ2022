da11e40b2a4335bb153549db25fc2163f9457a8e
Julio César  Gómez González <a01274966@itesm.mx>
2022-01-29 04:53:11 -0600

    Respawn and vfx test

--- GGJ2022KOTH/Assets/Scripts/PlayerController.cs
+++ GGJ2022KOTH/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,30 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform respawnPoint;
     public float speed = 5;
     private Vector2 movementInput;
+    public bool isDefeated = false;
 
     private void Update()
     {
         transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * speed * Time.deltaTime);
+
+
+        if (isDefeated)
+        {
+            speed = 0;
+            StartCoroutine(Respawn());
+        }
     }
 
     public void OnMove(InputAction.CallbackContext ctx) => movementInput = ctx.ReadValue<Vector2>();
+
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(2f);
+        gameObject.transform.position = respawnPoint.position;
+        speed = 5f;
+        isDefeated = false;
+    }
 }
